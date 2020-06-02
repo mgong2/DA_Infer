3bc5faf9fc38b74e16db0dd528ca3705844fe785
Mingming Gong <mgong@MacBook-Pro.local>
2020-06-02 16:32:42 +1000

    add wgan cifar net

--- code/models.py
+++ code/models.py
@@ -630,122 +630,6 @@ class PDAG_Generator(nn.Module):
         return result
 
 
-class CNN_Classifier(nn.Module):
-    def __init__(self, i_dim, cl_num, ch):
-        super(CNN_Classifier, self).__init__()
-        self.i_dim = i_dim
-        self.ch = ch
-        self.cl_num = cl_num
-
-        self.common_net = nn.Sequential(
-            nn.Conv2d(i_dim, ch, 4, 2, 1),
-            nn.BatchNorm2d(ch),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(ch, ch, 4, 2, 1),
-            nn.BatchNorm2d(ch),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(ch, ch, 3, 2, 1),
-            nn.BatchNorm2d(ch),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-        self.aux_c = nn.Linear(4 * 4 * ch, self.cl_num)
-
-    def forward(self, input0):
-        input = self.common_net(input0)
-        input = input.view(-1, 4 * 4 * self.ch)
-        output_c = self.aux_c(input)
-        return output_c
-
-
-class CNN_AuxClassifier(nn.Module):
-    def __init__(self, i_dim, cl_num, do_num, ch=64):
-        super(CNN_AuxClassifier, self).__init__()
-        self.i_dim = i_dim
-        self.ch = ch
-        self.cl_num = cl_num
-        self.do_num = do_num
-
-        self.common_net = nn.Sequential(
-            nn.Conv2d(i_dim, ch, 4, 2, 1),
-            nn.BatchNorm2d(ch),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(ch, ch, 4, 2, 1),
-            nn.BatchNorm2d(ch),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(ch, ch, 3, 2, 1),
-            nn.BatchNorm2d(ch),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-        self.aux_c = nn.Linear(4 * 4 * ch, self.cl_num)
-        self.aux_d = nn.Linear(4 * 4 * ch, self.do_num)
-        self.aux_c_tw = nn.Linear(4 * 4 * ch, self.cl_num)
-        self.aux_d_tw = nn.Linear(4 * 4 * ch, self.do_num)
-        self.disc = nn.Linear(4 * 4 * ch, 1)
-        self.cls = CNN_Classifier(i_dim, cl_num, ch)
-
-    def forward(self, input0):
-        input = self.common_net(input0)
-        input = input.view(-1, 4 * 4 * self.ch)
-        output_c = self.aux_c(input)
-        output_c_tw = self.aux_c_tw(input)
-        output_d = self.aux_d(input)
-        output_d_tw = self.aux_d_tw(input)
-        output_disc = self.disc(input)
-        output_cls = self.cls(input0)
-
-        return output_c, output_c_tw, output_d, output_d_tw, output_cls, output_disc
-
-
-# a CNN generator
-class CNN_Generator(nn.Module):
-    def __init__(self, i_dim, cl_num, do_num, cl_dim, do_dim, z_dim, ch=64, prob=True):
-        super(CNN_Generator, self).__init__()
-        self.prob = prob
-        self.do_dim = do_dim
-        self.ch = ch
-        if prob:
-            # VAE posterior parameters, Gaussian
-            self.mu = nn.Parameter(torch.zeros(do_num, do_dim))
-            self.sigma = nn.Parameter(torch.ones(do_num, do_dim))
-        else:
-            self.ld = nn.Linear(do_num, do_dim, bias=False)
-        self.lc = nn.Linear(cl_num, cl_dim, bias=False)
-
-        self.decoder1 = nn.Sequential(
-            nn.Linear(z_dim + cl_dim + do_dim, ch * 4 * 4),
-            nn.BatchNorm1d(ch * 4 * 4),
-            nn.ReLU(True),
-        )
-        self.decoder2 = nn.Sequential(
-            nn.ConvTranspose2d(ch, ch, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(ch),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(ch, ch, kernel_size=4, stride=2, padding=1),
-            nn.BatchNorm2d(ch),
-            nn.ReLU(True),
-            nn.ConvTranspose2d(ch, i_dim, kernel_size=4, stride=2, padding=1),
-            nn.Tanh()
-        )
-
-    def forward(self, noise, input_c, input_d, noise_d=None):
-        embed_c = self.lc(input_c)
-        if self.prob:
-            theta = self.mu + torch.mul(self.sigma, noise_d)
-            embed_d = torch.matmul(input_d, theta)
-        else:
-            embed_d = self.ld(input_d)
-        output = self.decoder1(torch.cat((embed_c, noise, embed_d), axis=1))
-        output = output.view(output.size(0), self.ch, 4, 4)
-        output = self.decoder2(output)
-        if self.prob:
-            KL_reg = 1 + torch.log(self.sigma**2) - self.mu**2 - self.sigma**2
-            if KL_reg.shape[1] > 1:
-                KL_reg = KL_reg.sum(axis=1)
-            return output, -KL_reg
-        else:
-            return output
-
-
 # a CNN generator
 class UNIT_Generator(nn.Module):
     def __init__(self, i_dim, cl_num, do_num, cl_dim, do_dim, z_dim, ch=64, prob=True):
@@ -866,9 +750,9 @@ class UNIT_AuxClassifier(nn.Module):
         return output_c, output_c_tw, output_d, output_d_tw, output_cls, output_disc
 
 
-class CNN_Classifier_Exp(nn.Module):
+class CNN_Classifier(nn.Module):
     def __init__(self, i_dim, cl_num, ch):
-        super(CNN_Classifier_Exp, self).__init__()
+        super(CNN_Classifier, self).__init__()
         self.i_dim = i_dim
         self.ch = ch
         self.cl_num = cl_num
@@ -893,9 +777,9 @@ class CNN_Classifier_Exp(nn.Module):
         return output_c
 
 
-class CNN_AuxClassifier_Exp(nn.Module):
+class CNN_AuxClassifier(nn.Module):
     def __init__(self, i_dim, cl_num, do_num, ch=64):
-        super(CNN_AuxClassifier_Exp, self).__init__()
+        super(CNN_AuxClassifier, self).__init__()
         self.i_dim = i_dim
         self.ch = ch
         self.cl_num = cl_num
@@ -911,6 +795,116 @@ class CNN_AuxClassifier_Exp(nn.Module):
             nn.Conv2d(ch * 2, ch * 4, 3, 2, 1),
             nn.BatchNorm2d(ch * 4),
             nn.LeakyReLU(0.2, inplace=True),
+        )
+        self.aux_c = nn.Linear(4*4*4*ch, self.cl_num)
+        self.aux_d = nn.Linear(4*4*4*ch, self.do_num)
+        self.aux_c_tw = nn.Linear(4*4*4*ch, self.cl_num)
+        self.aux_d_tw = nn.Linear(4*4*4*ch, self.do_num)
+        self.disc = nn.Linear(4*4*4*ch, 1)
+        self.cls = CNN_Classifier(i_dim, cl_num, ch)
+
+    def forward(self, input0):
+        input = self.common_net(input0)
+        input = input.view(-1, 4*4*4*self.ch)
+        output_c = self.aux_c(input)
+        output_c_tw = self.aux_c_tw(input)
+        output_d = self.aux_d(input)
+        output_d_tw = self.aux_d_tw(input)
+        output_disc = self.disc(input)
+        output_cls = self.cls(input0)
+
+        return output_c, output_c_tw, output_d, output_d_tw, output_cls, output_disc
+
+
+# a CNN generator
+class CNN_Generator(nn.Module):
+    def __init__(self, i_dim, cl_num, do_num, cl_dim, do_dim, z_dim, ch=64, prob=True):
+        super(CNN_Generator, self).__init__()
+        self.prob = prob
+        self.do_dim = do_dim
+        self.ch = ch
+        if prob:
+            # VAE posterior parameters, Gaussian
+            self.mu = nn.Parameter(torch.zeros(do_num, do_dim))
+            self.sigma = nn.Parameter(torch.ones(do_num, do_dim))
+        else:
+            self.ld = nn.Linear(do_num, do_dim, bias=False)
+        self.lc = nn.Linear(cl_num, cl_dim, bias=False)
+
+        self.decoder1 = nn.Sequential(
+            nn.Linear(z_dim + cl_dim + do_dim, ch*4*4*4),
+            nn.BatchNorm1d(ch*4*4*4),
+            nn.ReLU(True),
+        )
+        self.decoder2 = nn.Sequential(
+            nn.ConvTranspose2d(ch*4, ch*2, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(ch*2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ch*2, ch, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(ch),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ch, i_dim, kernel_size=4, stride=2, padding=1),
+            nn.Tanh()
+        )
+
+    def forward(self, noise, input_c, input_d, noise_d=None):
+        embed_c = self.lc(input_c)
+        if self.prob:
+            theta = self.mu + torch.mul(self.sigma, noise_d)
+            embed_d = torch.matmul(input_d, theta)
+        else:
+            embed_d = self.ld(input_d)
+        output = self.decoder1(torch.cat((embed_c, noise, embed_d), axis=1))
+        output = output.view(output.size(0), self.ch*4, 4, 4)
+        output = self.decoder2(output)
+        if self.prob:
+            KL_reg = 1 + torch.log(self.sigma**2) - self.mu**2 - self.sigma**2
+            if KL_reg.shape[1] > 1:
+                KL_reg = KL_reg.sum(axis=1)
+            return output, -KL_reg
+        else:
+            return output
+
+
+class CNN_Classifier_Exp(nn.Module):
+    def __init__(self, i_dim, cl_num, ch):
+        super(CNN_Classifier_Exp, self).__init__()
+        self.i_dim = i_dim
+        self.ch = ch
+        self.cl_num = cl_num
+
+        self.common_net = nn.Sequential(
+            nn.Conv2d(i_dim, ch, 3, 2, 1),
+            nn.LeakyReLU(),
+            nn.Conv2d(ch, ch*2, 3, 2, 1),
+            nn.LeakyReLU(),
+            nn.Conv2d(ch*2, ch*4, 3, 2, 1),
+            nn.LeakyReLU(),
+        )
+        self.aux_c = nn.Linear(4*4*4*ch, self.cl_num)
+
+    def forward(self, input0):
+        input = self.common_net(input0)
+        input = input.view(-1, 4*4*4*self.ch)
+        output_c = self.aux_c(input)
+        return output_c
+
+
+class CNN_AuxClassifier_Exp(nn.Module):
+    def __init__(self, i_dim, cl_num, do_num, ch=64):
+        super(CNN_AuxClassifier_Exp, self).__init__()
+        self.i_dim = i_dim
+        self.ch = ch
+        self.cl_num = cl_num
+        self.do_num = do_num
+
+        self.common_net = nn.Sequential(
+            nn.Conv2d(i_dim, ch, 3, 2, 1),
+            nn.LeakyReLU(),
+            nn.Conv2d(ch, ch * 2, 3, 2, 1),
+            nn.LeakyReLU(),
+            nn.Conv2d(ch * 2, ch * 4, 3, 2, 1),
+            nn.LeakyReLU(),
         )
         self.aux_c = nn.Linear(4*4*4*ch, self.cl_num)
         self.aux_d = nn.Linear(4*4*4*ch, self.do_num)
@@ -953,13 +947,13 @@ class CNN_Generator_Exp(nn.Module):
             nn.ReLU(True),
         )
         self.decoder2 = nn.Sequential(
-            nn.ConvTranspose2d(ch*4, ch*2, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(ch*4, ch*2, kernel_size=2, stride=2),
             nn.BatchNorm2d(ch*2),
             nn.ReLU(True),
-            nn.ConvTranspose2d(ch*2, ch, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(ch*2, ch, kernel_size=2, stride=2),
             nn.BatchNorm2d(ch),
             nn.ReLU(True),
-            nn.ConvTranspose2d(ch, i_dim, kernel_size=4, stride=2, padding=1),
+            nn.ConvTranspose2d(ch, i_dim, kernel_size=3, stride=2),
             nn.Tanh()
         )
 
@@ -971,7 +965,7 @@ class CNN_Generator_Exp(nn.Module):
         else:
             embed_d = self.ld(input_d)
         output = self.decoder1(torch.cat((embed_c, noise, embed_d), axis=1))
-        output = output.view(output.size(0), self.ch*4, 4, 4)
+        output = output.view(-1, self.ch*4, 4, 4)
         output = self.decoder2(output)
         if self.prob:
             KL_reg = 1 + torch.log(self.sigma**2) - self.mu**2 - self.sigma**2
